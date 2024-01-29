3d5f8ea3ad869ed5f17ceb587dd96ff7ae63762c
Kristina <kristina.tibeykina04@mail.ru>
2024-01-29 13:37:56 +0700

    рактическая работа №1

--- vue/js/main.js
+++ vue/js/main.js
@@ -2,6 +2,12 @@ let app = new Vue({
     el: '#app',
     data: {
         product: "Socks"
+
     }
 })
-
+let  description = new Vue({
+    el:'#description',
+    data: {
+        description: "A pair of warm, fuzzy socks"
+    }
+})
