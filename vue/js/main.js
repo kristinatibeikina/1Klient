589445250be63a9c0a180114047585fc58f29043
Kristina <kristina.tibeykina04@mail.ru>
2024-01-29 15:04:58 +0700

    Ьетод для изменения цвета фото при наведение на цвет

--- vue/js/main.js
+++ vue/js/main.js
@@ -7,22 +7,29 @@ let app = new Vue({
         inStock: true,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         cart: 0,
-        methods: {
-            addToCart() {
-                this.cart += 1
-            }
-        },
         variants: [
             {
                 variantId: 2234,
-                variantColor: 'green'
+                variantColor: 'green',
+                variantImage: "./assets/vmSocks-green-onWhite.jpg",
+
             },
             {
                 variantId: 2235,
-                variantColor: 'blue'
+                variantColor: 'blue',
+                variantImage: "./assets/vmSocks-blue-onWhite.jpg",
             }
         ],
-    }
+    },
+    methods: {
+        addToCart(){
+            this.cart += 1
+        },
+        updateProduct(variantImage) {
+            this.image = variantImage
+        }
+
+    },
 })
 
 
