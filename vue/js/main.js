d04d4d980169fd14106bd40f2e7d40a3aa15ccd7
Kristina <kristina.tibeykina04@mail.ru>
2024-01-29 14:02:22 +0700

    Добавляем изображение и alt

--- vue/js/main.js
+++ vue/js/main.js
@@ -1,13 +1,9 @@
 let app = new Vue({
     el: '#app',
     data: {
-        product: "Socks"
+        product: "Socks",
+        image: "./assets/vmSocks-green-onWhite.jpg",
+        altText: "A pair of socks"
+    }
+})
 
-    }
-})
-let  description = new Vue({
-    el:'#description',
-    data: {
-        description: "A pair of warm, fuzzy socks"
-    }
-})
