d8197a00492ea2c1afc6c0c5438c1a1648d9b880
Kristina <kristina.tibeykina04@mail.ru>
2024-01-30 12:54:03 +0700

    Создание объектов на основе введенных пользователем данных

--- vue/js/main.js
+++ vue/js/main.js
@@ -1,3 +1,5 @@
+
+
 Vue.component('product', {
     props: {
         premium: {
@@ -98,6 +100,64 @@ Vue.component('product', {
         }
     }
 })
+
+Vue.component('product-review', {
+    template: `
+   <form class="review-form" @submit.prevent="onSubmit">
+ <p>
+   <label for="name">Name:</label>
+   <input id="name" v-model="name" placeholder="name">
+ </p>
+
+ <p>
+   <label for="review">Review:</label>
+   <textarea id="review" v-model="review"></textarea>
+ </p>
+
+ <p>
+   <label for="rating">Rating:</label>
+   <select id="rating" v-model.number="rating">
+     <option>5</option>
+     <option>4</option>
+     <option>3</option>
+     <option>2</option>
+     <option>1</option>
+   </select>
+ </p>
+
+ <p>
+   <input type="submit" value="Submit"> 
+ </p>
+
+</form>
+
+ `,
+    data() {
+        return {
+            name: null,
+            review: null,
+            rating: null
+        }
+    },
+    methods:{
+        onSubmit() {
+            let productReview = {
+                name: this.name,
+                review: this.review,
+                rating: this.rating
+            }
+            this.$emit('review-submitted', productReview)
+            this.name = null
+            this.review = null
+            this.rating = null
+        }
+    }
+
+
+
+})
+
+
 let app = new Vue({
     el: '#app',
     data: {
